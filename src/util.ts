afd0146095868e4f321e7c644be4a95a91672961
Yuki-Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2023-02-04 15:10:40 +0900

    test

--- src/util.ts
+++ src/util.ts
@@ -2,7 +2,7 @@ export class User {
   name: string;
   age: number;
 
-  constructor(name: number, age: number) {
+  constructor(name: string, age: number) {
     this.name = name;
     this.age = age;
   }
