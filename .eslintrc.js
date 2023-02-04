1a5e5c9ed2634172efc7b4aec444a111990ef3b7
Yuki-Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2023-02-04 14:00:44 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: ['plugin:import/recommended', 'plugin:import/warnings', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['import', '@typescript-eslint'],
+  root: true,
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'parent', 'sibling', 'index', 'object', 'type'],
+        pathGroups: [
+          {
+            pattern: '@src/**',
+            group: 'parent',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+        alphabetize: {
+          order: 'asc',
+        },
+        'newlines-between': 'always',
+      },
+    ],
+    '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
+  },
+};
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { User } from './util';
+
+const me: User = new User('yuki', 31);
+console.log(me.name, me.age, me.constructor.name);
